32262b2d5fadb6a91cdaf5d4ec731b17ae54aac0
Schude <nemli.serkan2@gmail.com>
2021-02-13 01:42:52 +0300

    First Commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const electron = require("electron");
+const { ipcRenderer } = electron;
+
+const NAME_OF_ITEMS = [
+  "Death Blossom",
+  "Marrowroot",
+  "Rising Glory",
+  "Vigil's Torch",
+  "Widowbloom",
+  "Nightshade",
+  "Luminous Pigment",
+  "Umbral Pigment",
+  "Tranquil Pigment",
+  "Luminous Ink",
+  "Umbral Ink",
+  "Tranquil Ink",
+  "Aerated Water",
+  "Rune Etched Vial",
+];
+const itemContainer = document.querySelector(".item-list");
+
+for (i = 0; i < NAME_OF_ITEMS.length; i++) {
+  const item = document.createElement("div");
+  item.classList.add("item-list-items");
+  itemContainer.appendChild(item)
+  let itemName = document.createElement("div");
+  itemName.classList.add("item-name");
+  let text = document.createTextNode(NAME_OF_ITEMS[i]);
+  itemName.appendChild(text);
+  item.appendChild(itemName);
+  const inputs = document.createElement("input");
+  item.appendChild(inputs);
+  
+}
+
+// console.log(containerItem);
+// const inputs = document.createElement("input");
+// const names = document.createElement("div");
+// const nameText = document.createTextNode("DeathBsssdasdlossom");
+// containerItem.appendChild(names);
+// const inputs = document.createElement("input");
+// containerItem.appendChild(inputs);
+// itemContainer.appendChild(containerItem);
---
+++ main.js
@@ -0,0 +1,76 @@
+const electron = require("electron");
+
+require("electron-reload")(__dirname, {
+  // Note that the path to electron may vary according to the main file
+  electron: require(`${__dirname}/node_modules/electron`),
+});
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("index.html");
+}
+app.whenReady().then(createWindow);
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+ipcMain.on("key:input", (err, data) => {
+  console.log(data);
+});
+const mainMenuTemplate = [
+  {
+    label: "Menuss",
+    submenu: [{ label: "1" }, { label: "2" }],
+  },
+  {
+    label: "Dev Tools",
+    submenu: [
+      {
+        label: "Dev Tool",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+      {
+        label: "Reload",
+        role: "reload",
+        accelerator: process.platform !== "darwin" ? "Ctrl+R" : "Command+R",
+      },
+    ],
+  },
+];
+
+const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+Menu.setApplicationMenu(mainMenu);
+
+ipcMain.on("key:newWin", () => {
+  createWin();
+});
+
+function createWin() {
+  let addWin = new BrowserWindow({
+    width: 480,
+    height: 360,
+    title: "New Winssdsow",
+  });
+  addWin.loadFile("modal.html");
+  addWin.on("close", () => {
+    addWin = null;
+  });
+}
