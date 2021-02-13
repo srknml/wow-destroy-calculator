9b0e3fd14f69bf39b172aec910612de476cee585
Schude <nemli.serkan2@gmail.com>
2021-02-13 23:27:05 +0300

    2. part JS ile Yazıldı. Style eklendi.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const NAME_OF_ITEMS = [
 ];
 const itemContainer = document.querySelector(".item-list");
 
-for (i = 0; i < NAME_OF_ITEMS.length; i++) {
+for (let i = 0; i < NAME_OF_ITEMS.length; i++) {
   const item = document.createElement("div");
   item.classList.add("item-list-items");
   itemContainer.appendChild(item);
@@ -30,4 +30,34 @@ for (i = 0; i < NAME_OF_ITEMS.length; i++) {
   item.appendChild(itemName);
   const inputs = document.createElement("input");
   item.appendChild(inputs);
+}
+
+const totalContainer = document.querySelector(".total-cost");
+
+for (let i = 0; i < 3; i++) {
+  const container = document.createElement("div");
+  container.classList.add("total-cost-container");
+
+  const title = document.createElement("h4");
+  title.classList.add("container-title");
+  let titleText = document.createTextNode("titleText");
+  title.appendChild(titleText);
+  container.appendChild(title);
+
+  for (let i = 9; i < 13; i++) {
+    const items = document.createElement("div");
+    items.classList.add("total-cost-items");
+
+    const itemName = document.createElement("div");
+    itemName.classList.add("item-name");
+    itemName.appendChild(document.createTextNode(NAME_OF_ITEMS[i]));
+    items.appendChild(itemName);
+
+    const itemCost = document.createElement("div");
+    itemCost.classList.add("item-cost");
+    items.appendChild(itemCost);
+    container.appendChild(items);
+  }
+
+  totalContainer.appendChild(container);
 }
