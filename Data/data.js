bb624317111f367ed8b708fb0ec87aa43ca40d1f
Schude <nemli.serkan2@gmail.com>
2021-02-15 02:31:08 +0300

    slug'a göre realm ID'ye ulaştık

--- Data/data.js
+++ Data/data.js
@@ -2,51 +2,56 @@ require("dotenv").config();
 const fetch = require("node-fetch");
 
 class DataProvider {
-  constructor(oauthClient,userConfig) {
+  constructor(oauthClient, user) {
     this.oauthClient = oauthClient;
-    this.userConfig = userConfig
+    this.user = user;
   }
 
-  async getConnectedRealmId(realmSlug, region) {
+  async getConnectedRealmId() {
     const token = await this.oauthClient.getToken();
     console.log("DataProvider ", token);
+    console.log(this.user.region);
+    console.log(this.user.realm);
+    const BLIZZ_CONNECTED_REALM_INDEX_URL = `https://${this.user.region}.api.blizzard.com/data/wow/connected-realm/index`;
+    const BATTLENET_NAMESPACE = `dynamic-${this.user.region}`;
+
+    var response = await fetch(BLIZZ_CONNECTED_REALM_INDEX_URL, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+        "Battlenet-Namespace": BATTLENET_NAMESPACE,
+      },
+    });
+    const data = await response.json();
+    const realmList = data.connected_realms;
+
+    for (let i = 0; i < realmList.length; i++) {
+      const realmData = await fetch(realmList[i].href, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + token,
+          "Battlenet-Namespace": BATTLENET_NAMESPACE,
+        },
+      });
+      //
+      const connectedRealmsData = await realmData.json();
+      const realmId = connectedRealmsData.id;
+      for (let j = 0; j < connectedRealmsData.realms.length; j++) {
+        console.log("FOR'a Girdi");
+        let currSlug = connectedRealmsData.realms[j].slug;
+        if (currSlug == this.user.realm) {
+          return realmId;
+        }
+      }
+    }
   }
+
+
+
+
+
+
+  
 }
 
 module.exports = DataProvider;
-
-// const token = await  OauthClient.getToken();
-// const token = accessToken;
-// const region = "eu"; //Userdan Alınacak
-// const realmSlug = "silvermoon"; //Userdan Alınacak çevirlip alınacak
-// console.log(token, "TOKEN");
-// async function getConnectedRealmId(realmSlug, region, token) {
-//   const BLIZZ_CONNECTED_REALM_INDEX_URL = `https://${region}.api.blizzard.com/data/wow/connected-realm/index`;
-//   const BATTLENET_NAMESPACE = `dynamic-${region}`;
-
-//   var response = await fetch(BLIZZ_CONNECTED_REALM_INDEX_URL, {
-//     method: "GET",
-//     headers: {
-//       Authorization: "Bearer " + token,
-//       "Battlenet-Namespace": BATTLENET_NAMESPACE,
-//     },
-//   });
-//   const data = await response.json();
-
-//   const realmList = data.connected_realms;
-//   console.log(realmList, "  Realm List");
-//   const realmData = await fetch(realmList[20].href, {
-//     method: "GET",
-//     headers: {
-//       Authorization: "Bearer " + token,
-//       "Battlenet-Namespace": BATTLENET_NAMESPACE,
-//     },
-//   });
-//   const x = await realmData.json();
-
-//   console.log(
-//     x.auctions.href + "&access_token=USE6oKD6mxSDJv8kmwjsk3JSjlwKJ2o38z"
-//   ); //Datanın Çekileceği Yer
-// }
-
-// getConnectedRealmId(realmSlug, region, token);
